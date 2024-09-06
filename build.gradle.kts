2006b8bd9f93561771c5389a88e0d639d8f1ab44
Conal Smith <conal.smith@r3.com>
2024-09-06 12:27:10 +0100

    some publication stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
     `kotlin-dsl`
+    `maven-publish`
+    id("com.jfrog.artifactory") version "5.2.5"
     id("com.r3.gradle.standards.kotlin-common")
-    id("com.r3.gradle.standards.publication.maven-publication")
+//    id("com.r3.gradle.standards.publication.maven-publication") version "0.2.15-bm_r3sol-649_updating-a9y-defaults.96896e3-SNAPSHOT"
+//    id("com.r3.gradle.standards.publication.terraform-publication") version "0.3.0-r3sol-364.a2028d6-SNAPSHOT"
 }
 
 group = "com.r3.example"
@@ -24,7 +27,7 @@ repositories {
 }
 
 group = "com.r3.example"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.3-SNAPSHOT"
 
 publishing {
     publications {
@@ -36,4 +39,51 @@ publishing {
 
 dependencies {
     testImplementation(libs.bundles.test)
+}
+
+//terraform {
+//    modules = mapOf(
+//        "terraform-zip" to "$projectDir/terraform"
+//    )
+//    provider = "kubernetes"
+//    repositoryKey = "terraform-modules-dev"
+//    tag = "conal-123"
+//    namespace = "abc"
+//}
+
+
+artifactory {
+    publish {
+        val props = (extra.properties + System.getenv()).mapKeys { it.key.lowercase().replace("_", "").replace("corda","") }
+        contextUrl = props["artifactoryUrl".lowercase()]?.toString() ?: ""
+
+        repository {
+            username = props["artifactoryUsername".lowercase()]?.toString() ?: ""
+            password = props["artifactoryPassword".lowercase()]?.toString() ?: ""
+            repoKey = props["artifactoryRepositoryKey".lowercase()]?.toString() ?: ""
+        }
+
+        defaults {
+            setPublishPom(true)
+            setPublishArtifacts(true)
+            isPublishBuildInfo = true
+            publications("ALL_PUBLICATIONS")
+        }
+    }
+}
+
+sourceSets {
+    main {
+        kotlin.srcDirs("src/main/kotlin")
+        resources.srcDirs("src/main/resources")
+    }
+}
+
+java {
+    withSourcesJar()
+}
+
+// TODO: remove once R3SOL-298 is implemented.
+tasks.named("publish") {
+    finalizedBy("artifactoryPublish")
 }
